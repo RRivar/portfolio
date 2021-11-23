703b70f8f9524f9a0b3965403533810adc8a6d6b
RRivar <takitam132@gmail.com>
2021-11-23 03:48:21 +0100

    init + firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Cards from './components/Cards';
+import { Container } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <Cards />
+          </Grid>
+          <Grid item xs={4}>
+            <Cards />
+          </Grid>
+          <Grid item xs={4}>
+            <Cards />
+          </Grid>
+          <Grid item xs={4}>
+            <Cards />
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 }
 
